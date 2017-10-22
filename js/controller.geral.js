73787baa0d2a357b449f0ccc9407411b88772178
idea-PC\Patricia <patriciasoramos@gmail.com>
2017-10-22 11:15:12 -0200

    Validations

--- js/controller.geral.js
+++ js/controller.geral.js
@@ -30,6 +30,42 @@ app.controller('ctrlGeral', function($scope,$http) {
 		$("#primeiro").removeClass("current");
 	}
 
+	$scope.valida = function(){
+
+		$scope.erros = [];
+
+		if($("#nr_cartao").val() == ""){
+			$scope.erros.push("Número do cartão")
+		}else{
+			var regexCartao = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})|((((636368)|(438935)|(504175)|(451416)|(636297))\d{0,10})|((5067)|(4576)|(4011))\d{0,12})|(606282\d{10}(\d{3})?)|(3841\d{15})$/;
+			if(!$("#nr_cartao").val().match(regexCartao)){
+				$scope.erros.push("Número do cartão - digite um cartão válido")
+			}
+		}
+
+		if($("#mes").val() == ""){
+			$scope.erros.push("Mês")
+		}
+
+		if($("#ano").val() == ""){
+			$scope.erros.push("Ano")
+		}
+
+		if($("#nome_titular").val() == ""){
+			$scope.erros.push("Nome do titular")
+		}
+
+		if($("#cod_seguranca").val() == ""){
+			$scope.erros.push("Código de segurança")
+		}else{
+
+		}
+
+		if($scope.erros.length == 0){
+			$scope.showProxPasso=false;
+			$scope.showFinal=true;
+		}		
+	}
 
 
 	$scope.meses = {
@@ -62,3 +98,9 @@ app.controller('ctrlGeral', function($scope,$http) {
 $( document ).ready(function() {
 	$("#div1").addClass("selectedRadio");
 });
+
+function upperCaseF(a){
+    setTimeout(function(){
+        a.value = a.value.toUpperCase();
+    }, 1);
+}
